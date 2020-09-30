28f5ac81bd97c636aee0bd2af99f7bc5a4199399
gennadykh <gennadykh@gmail.com>
2020-09-30 17:26:46 -0500

    Add project files.

---
+++ Migrations/20200928235307_initial_migration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorServerSideCRUD.Migrations
+{
+    public partial class initial_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    School = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "FirstName", "LastName", "School" },
+                values: new object[,]
+                {
+                    { "4fa9fcf2-777d-4692-993d-5d2ff6ccb4d6", "Jane", "Smith", "Medicine" },
+                    { "85c0d3a2-0718-4940-8e84-f85cdc29be8f", "John", "Fisher", "Engineering" },
+                    { "3c397645-75d8-488a-ac6b-3b06bc1ee3e9", "Pamela", "Baker", "Food Science" },
+                    { "52bbcd5d-105c-4926-bdf0-f929d54ee2e5", "Peter", "Taylor", "Mining" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerSideCRUD.Models
+{
+    public class Student
+    {
+        public string StudentId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string School { get; set; }
+    }
+}
